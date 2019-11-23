154a2743cacbfd13d0cb6fa30efa32c4f4978ac3
Ahmad Hanif Nurfauzi <hanif_a13@yahoo.com>
2019-11-24 04:48:40 +0700

    Push

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    public Text countText;
+    private int count;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            setCountText();
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count : " + count.ToString();
+        if (count >= 12)
+        {
+            countText.text = "You Win!";
+        }
+    }
+}
